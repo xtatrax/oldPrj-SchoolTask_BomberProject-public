30648a97ced2a39c9fc317b9a55b9e5847827c0b
tatra <xtatrax@gmial.com>
2013-05-16 12:02:32 +0900

    Stage1用のファクトリーを用意した

--- BomberProject/Factory_Stage1.cpp
+++ BomberProject/Factory_Stage1.cpp
@@ -10,7 +10,9 @@
 //		class Factory_Stage1 ;
 //
 #include "StdAfx.h"
+#include "Stage.h"
 #include "Factory_Stage1.h"
+#include "Factory_Player.h"
 
 namespace wiz{
 
@@ -30,42 +32,65 @@ namespace wiz{
 ***************************************************************************/
 Factory_Stage1::Factory_Stage1(FactoryPacket *fpac){
 	try{
-		////カメラのインスタンス初期化
-		//float ECXPos = 25.1f;
-		//float ECYPos = 10.1f;		
-  //      m_Vec.push_back(
-		//	new Camera(pD3DDevice,D3DXVECTOR3( ECXPos, ECYPos, -55.7f),D3DXVECTOR3(ECXPos,ECYPos,0.0f), 1 ,300.0f,30.0f)
-		//);
-		////	: ガイドライン
-		//m_Vec.push_back(new Guide( pD3DDevice ) );
+		LPDIRECT3DDEVICE9 pD3DDevice	= fpac->pD3DDevice	;
+		vector<Object*>*  pVec			= fpac->m_pVec		;
 
-		//MapPartsStatus MapData[] = {
-		//	{ OBJID_3D_WALL, D3DXVECTOR3( 2.0f, 2.0f, 0.0f), g_vZero, g_vZero, getD3DCOLORVALUE( 0.5f, 0.5f, 0.5f, 0.5f), getD3DCOLORVALUE( 0.0f, 0.0f, 0.0f, 0.0f), getD3DCOLORVALUE( 0.7f, 0.7f, 0.7f, 0.7f) },
-		//	{ OBJID_END }
-		//
-		//};
+		//カメラのインスタンス初期化
+		float ECXPos = 25.1f;
+		float ECYPos = 10.1f;
+		pVec->push_back(
+			new Camera(pD3DDevice,D3DXVECTOR3( ECXPos, ECYPos, -55.7f),D3DXVECTOR3(ECXPos,ECYPos,0.0f), 1 ,300.0f,30.0f)
+		);
+		//	: ガイドライン
+		pVec->push_back(new Guide( pD3DDevice ) );
+
+		/*
+			DWORD			enClassid ;	//	wiz::CLASSIDで指定してください
+			D3DXVECTOR3		vScale    ;
+			D3DXVECTOR3		vRot      ;
+			D3DXVECTOR3		vPos      ;
+			D3DCOLORVALUE   Diffuse   ;
+			D3DCOLORVALUE   Specular  ;
+			D3DCOLORVALUE   Ambient   ;
+			bool			bPool     ;
+
+			//	: オプション
+			wstring			sTexturePath	;
+
+			wstring			sFilePath		;
+			DWORD			dwMotionNum		;
+			float			fTracSpeed		;
+
+		*/
+		MapPartsStatus MapData[] = {
+			{ OBJID_3D_WALL,	 D3DXVECTOR3( 2.0f, 2.0f, 0.0f), g_vZero, g_vZero, getD3DCOLORVALUE( 0.5f, 0.5f, 0.5f, 0.5f), getD3DCOLORVALUE( 0.0f, 0.0f, 0.0f, 0.0f), getD3DCOLORVALUE( 0.7f, 0.7f, 0.7f, 0.7f) },
+			{ OBJID_3D_ITEM,	 D3DXVECTOR3( 2.0f, 2.0f, 0.0f), g_vZero, g_vZero, getD3DCOLORVALUE( 0.5f, 0.5f, 0.5f, 0.5f), getD3DCOLORVALUE( 0.0f, 0.0f, 0.0f, 0.0f), getD3DCOLORVALUE( 0.7f, 0.7f, 0.7f, 0.7f) },
+			{ OBJID_3D_MAGNET,	 D3DXVECTOR3( 2.0f, 2.0f, 0.0f), g_vZero, g_vZero, getD3DCOLORVALUE( 0.5f, 0.5f, 0.5f, 0.5f), getD3DCOLORVALUE( 0.0f, 0.0f, 0.0f, 0.0f), getD3DCOLORVALUE( 0.7f, 0.7f, 0.7f, 0.7f) },
+			{ OBJID_END }
+		
+		};
 
 
-		//StageLoader loader(pD3DDevice,m_Vec,m_TexMgr,MapData);
+		StageLoader loader(pD3DDevice,*pVec,*fpac->m_pTexMgr,MapData);
 
 
-		//Factory_Player Pfac( &FPac );
+		Factory_Player Pfac( fpac );
 
 
-		////	: スプライト
-		//fpac->m_pVec->push_back(
-		//	new SpriteObject(
-		//		fpac->pD3DDevice,
-		//		fpac->m_pTexMgr->addTexture( fpac->pD3DDevice, L"display.png" ),
-		//		g_vOne,
-		//		g_vZero,
-		//		g_vZero,
-		//		NULL,
-		//		g_vZero,
-		//		g_vZero,
-		//		0xFFFFFFFF
-		//	)
-		//);
+		//	: スプライト
+		pVec->push_back(
+			new SpriteObject(
+				pD3DDevice,
+				fpac->m_pTexMgr->addTexture( fpac->pD3DDevice, L"display.png" ),
+				g_vOne,
+				g_vZero,
+				g_vZero,
+				NULL,
+				g_vZero,
+				g_vZero,
+				0xFFFFFFFF
+			)
+		);
 
 
 	}
