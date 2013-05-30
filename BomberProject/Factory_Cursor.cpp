f857a9df88be03178a13ea2a13f41930380de68f
tatra <xtatrax@gmial.com>
2013-05-30 16:43:04 +0900

    ソフトウエアカーソルを実装した｡ サウンドを改良した｡

--- BomberProject/Factory_Cursor.cpp
+++ BomberProject/Factory_Cursor.cpp
@@ -30,49 +30,77 @@ namespace wiz{
  担当：本多寛之
 ***************************************************************************/
 MouseCursor::MouseCursor(
-						 LPDIRECT3DDEVICE9 pD3DDevice,
-						 LPDIRECT3DTEXTURE9 pTexture,
-						 D3DXVECTOR3	   &vScale,
-						 D3DXVECTOR3	   &vPos,
-						 wiz::OBJID id)					 
+		LPDIRECT3DDEVICE9 pD3DDevice,
+		TextureManager* m_pTexMgr)
 :PrimitiveBox(pD3DDevice,
-			  D3DCOLORVALUE(),
-			  D3DCOLORVALUE(),
-			  D3DCOLORVALUE(),
-			  id,
-		      pTexture)
-,m_pMouseTex( pTexture )
+			  COLOR2D3DCOLORVALUE(0),
+			  COLOR2D3DCOLORVALUE(0),
+			  COLOR2D3DCOLORVALUE(0),
+			  OBJID_3D_CURSOR)
+,PrimitiveSprite(pD3DDevice, m_pTexMgr->addTexture( pD3DDevice, L"MouseCursor.png" ), NULL, D3DXVECTOR3( 22.0f, 22.0f, 0.0f ), g_vZero)
+,m_pMouseTex( m_pTexMgr->addTexture( pD3DDevice, L"MouseCursor.png" ) )
 ,m_Ptn(0)
+,m_MovePosY(0)
 {
-	::ZeroMemory( &m_Material, sizeof(D3DMATERIAL9));
+
+	D3DXVECTOR3 vScale = D3DXVECTOR3(0.5f,0.5f,0.0f);
+	D3DXMatrixScaling( &m_mScale, vScale.x, vScale.y, vScale.z );
+	PrimitiveBox::SetBaseScale( vScale );
 	m_pCamera = NULL;
 
 }
+/////////////////// ////////////////////
+//// 用途       ：void Update( UpdatePacket& i_UpdatePacket )
+//// カテゴリ   ：関数
+//// 用途       ：オブジェクトを更新
+//// 引数       ：  UpdatePacket& i_UpdatePacket     // アップデート時に必要なデータ群 ↓内容下記
+////            ：  ├       LPDIRECT3DDEVICE9  pD3DDevice      // IDirect3DDevice9 インターフェイスへのポインタ
+////            ：  ├       Tempus2*           pTime           // 時間を管理するクラスへのポインター
+////            ：  ├       vector<Object*>&   Vec,            // オブジェクトの配列
+////            ：  ├ const CONTROLER_STATE*   pCntlState      // コントローラのステータス
+////            ：  └       Command            pCommand        // コマンド
+//// 戻値       ：無し
+//// 担当者     ：鴫原 徹
+//// 備考       ：
+////            ：
+////
+void MouseCursor::Update( UpdatePacket& i_UpdatePacket ){
+	if(m_pCamera == NULL){
+		m_pCamera = (Camera*)SearchObjectFromID(i_UpdatePacket.pVec,OBJID_SYS_CAMERA);
+		m_pCamera && (m_MovePosY	= m_pCamera->getPosY());
+	}
 
-///////////////////// ////////////////////
-////// 用途       ：MouseCursor(	LPDIRECT3DDEVICE9 pD3DDevice,LPDIRECT3DTEXTURE9 pTexture,wiz::OBJID id = OBJID_3D_WALL);
-////// カテゴリ   ：コンストラクタ
-////// 用途       ：関数
-////// 引数       ：なし
-////// 戻値       ：なし
-////// 担当者     ：鴫原 徹
-////// 備考       ：
-//void MouseCursor::UpdateTargetItem(){
-	
-	
-//}
 
+	//	: カーソルの設定
+	//	: マウスのクライアント座標を獲得
+	Point MousePos ;
+	GetCursorPos( &MousePos ) ;
+	ScreenToClient( g_hWnd , &MousePos) ;
 	
-//bool MouseCursor::HitTest2DRectAndCircle(D3DXVECTOR3& i_vPos, float i_fRadius)
-//{
-//	//float fTH = 150 ;
-//	//float fTW =  20 ;
-//	//if( abs(m_vPos.x - i_vPos.x) < fTW + i_fRadius &&
-//	//	abs(m_vPos.y - i_vPos.y) < fTH + i_fRadius ){
-//	//	return true ;
-//	//}
-//	return false ;
-//}
+	D3DXMATRIX mPos ;
+	D3DXMatrixTranslation( &mPos, (float)MousePos.x, (float)MousePos.y, 0.0f);
+
+	m_mMatrix = m_mScale * mPos ;
+
+
+	if( m_pCamera ){
+
+		//	: マウス座標の３Ｄ変換
+		D3DXVECTOR3 vCursol = D3DXVECTOR3( 
+			(float)MousePos.x / DRAW_CLIENT_MAGNIFICATION - MAGNETIC_RADIUS ,
+			(( STANDARD_WINDOW_HEIGHT - MousePos.y ) - UI_HEIGHT ) / DRAW_CLIENT_MAGNIFICATION -
+											MAGNETIC_RADIUS + ( m_pCamera->getPosY() - m_MovePosY ) ,
+			0.0f
+		);
+		SetBasePos( vCursol );
+
+		Debugger::DBGSTR::addStr( L" Pos( %f, %f, %f )\n" , vCursol.x , vCursol.y, vCursol.z ) ;
+
+		CalcWorldMatrix();
+
+	}
+	++m_Ptn;
+}
 
 /////////////////// ////////////////////
 //// 用途       ：void Draw( DrawPacket& i_DrawPacket )
@@ -88,143 +116,10 @@ MouseCursor::MouseCursor(
 //// 備考       ：
 void MouseCursor::Draw(DrawPacket& i_DrawPacket)
 {
-	//multimap<float,WallItem*>::iterator it = m_ItemMap_Target.begin();
-	//while(it != m_ItemMap_Target.end()){
-	//	++it;
-	//}
-	//multimap<float,PolyItem*>::iterator it2 = m_ItemMap_Poly.begin();
-	//while(it2 != m_ItemMap_Poly.end()){
-	//	//テクスチャがある場合
-	//	if(m_pMouseTex){
-	//		m_pTexture	= m_pMouseTex;
-	//		DWORD wkdword;
-	//		//現在のテクスチャステータスを得る
-	//		i_DrawPacket.pD3DDevice->GetTextureStageState(0,D3DTSS_COLOROP,&wkdword);
-	//		//ステージの設定
-	//		i_DrawPacket.pD3DDevice->SetTexture(0,m_pTexture);
-	//		//デフィーズ色とテクスチャを掛け合わせる設定
-	//		i_DrawPacket.pD3DDevice->SetTextureStageState( 0, D3DTSS_COLOROP, D3DTOP_MODULATE4X );
-	//		i_DrawPacket.pD3DDevice->SetTextureStageState( 0, D3DTSS_COLORARG1, D3DTA_TEXTURE );
-	//		i_DrawPacket.pD3DDevice->SetTextureStageState( 0, D3DTSS_COLORARG2, D3DTA_DIFFUSE );
-
-	//		//i_DrawPacket.pD3DDevice->SetFVF(PlateFVF);
-	//		// マトリックスをレンダリングパイプラインに設定
-	//		i_DrawPacket.pD3DDevice->SetTransform(D3DTS_WORLD, &it2->second->m_Matrix);
-	//		//コモンメッシュのDraw()を呼ぶ
-	//		CommonMesh::Draw(i_DrawPacket);
-	//		i_DrawPacket.pD3DDevice->SetTexture(0,0);
-	//		//ステージを元に戻す
-	//		i_DrawPacket.pD3DDevice->SetTextureStageState(0,D3DTSS_COLOROP,wkdword);
-	//	}
-	//	else{
-	//	//テクスチャがない場合
-	//		// マトリックスをレンダリングパイプラインに設定
-	//		i_DrawPacket.pD3DDevice->SetTransform(D3DTS_WORLD, &it2->second->m_Matrix);
-	//		//コモンメッシュのDraw()を呼ぶ
-	//		CommonMesh::Draw(i_DrawPacket);
-	//	}
-
-	//	++it2;
-	//}
+	PrimitiveSprite::Draw(i_DrawPacket);
+	PrimitiveBox::Draw(i_DrawPacket);
 }
 
-/////////////////// ////////////////////
-//// 用途       ：void Update( UpdatePacket& i_UpdatePacket )
-//// カテゴリ   ：関数
-//// 用途       ：オブジェクトを更新
-//// 引数       ：  UpdatePacket& i_UpdatePacket     // アップデート時に必要なデータ群 ↓内容下記
-////            ：  ├       LPDIRECT3DDEVICE9  pD3DDevice      // IDirect3DDevice9 インターフェイスへのポインタ
-////            ：  ├       Tempus2*           pTime           // 時間を管理するクラスへのポインター
-////            ：  ├       vector<Object*>&   Vec,            // オブジェクトの配列
-////            ：  ├ const CONTROLER_STATE*   pCntlState      // コントローラのステータス
-////            ：  └       Command            pCommand        // コマンド
-//// 戻値       ：無し
-//// 担当者     ：本多寛之
-//// 備考       ：
-////            ：
-////
-void MouseCursor::Update( UpdatePacket& i_UpdatePacket ){
-	if(m_pCamera == NULL){
-		m_pCamera = (Camera*)SearchObjectFromID(i_UpdatePacket.pVec,OBJID_SYS_CAMERA);
-	}
-	//if(m_pPlayerCoil == NULL){
-	//	m_pPlayerCoil = (PlayerCoil*)SearchObjectFromTypeID(i_UpdatePacket.pVec,typeid(PlayerCoil));
-	//}
-
-	//m_ItemMap_Target.clear();
-	//multimap<float,WallItem*>::iterator it = m_ItemMap_All.begin();
-	//while(it != m_ItemMap_All.end()){
-	//	if( ( +(it->first - m_pCamera->getEye().y) <= DRAWING_RANGE) && ( +(it->first - m_pCamera->getEye().y) >= -DRAWING_RANGE ) ){
-	//		m_ItemMap_Target.insert(multimap<float,WallItem*>::value_type(it->second->m_vPos.y,it->second));
-	//	}
-	//	++it;
-	//}
-
-	//multimap<float,CursorItem*>::iterator it2 = m_ItemMap_Target.begin();
-	//while(it2 != m_ItemMap_Target.end()){
-	//	//計算はUpdateで
-	//	//拡大縮小
-	//	D3DXMATRIX mScale;
-	//	D3DXMatrixIdentity(&mScale);
-	//	D3DXMatrixScaling(&mScale,
-	//		it2->second->m_vScale.x,it2->second->m_vScale.y,it2->second->m_vScale.z);
-	//	//回転
-	//	D3DXMATRIX mRot;
-	//	D3DXMatrixIdentity(&mRot);
-	//	D3DXMatrixRotationQuaternion(&mRot,&it2->second->m_vRot);
-	//	//移動用
-	//	D3DXMATRIX mMove;
-	//	D3DXMatrixIdentity(&mMove);
-	//	D3DXMatrixTranslation(&mMove,
-	//		it2->second->m_vPos.x,it2->second->m_vPos.y,it2->second->m_vPos.z);
-	//	//ミックス行列
-	//	it2->second->m_Matrix = mScale * mRot * mMove;
-	//	//マティリアル設定
-	//	m_Material = it2->second->m_Material;
-
-	//	++it2;
-	//}
-
-	//multimap<float,PolyItem*>::iterator it3 = m_ItemMap_Poly.begin();
-	//while(it3 != m_ItemMap_Poly.end()){
-	//	//計算はUpdateで
-	//	//拡大縮小
-	//	D3DXMATRIX mScale;
-	//	D3DXMatrixIdentity(&mScale);
-	//	D3DXMatrixScaling(&mScale,
-	//		it3->second->m_vScale.x,it3->second->m_vScale.y,0.0f);
-	//	//回転
-	//	D3DXMATRIX mRot;
-	//	D3DXMatrixIdentity(&mRot);
-	//	D3DXMatrixRotationQuaternion(&mRot,&it3->second->m_vRot);
-	//	//移動用
-	//	D3DXMATRIX mMove;
-	//	D3DXMatrixIdentity(&mMove);
-	//	D3DXMatrixTranslation(&mMove,
-	//		it3->second->m_vPos.x,it3->second->m_vPos.y,it3->second->m_vPos.z);
-	//	//ミックス行列
-	//	it3->second->m_Matrix = mScale * mRot * mMove;
-	//	//マティリアル設定
-	//	m_Material = it3->second->m_Material;
-
-	//	++it3;
-	//}
-
-	//**************************************************************************************
-	LPDIRECT3DVERTEXBUFFER9 pVB = 0;
-	CommonMeshVertex* pVer = 0;
-	m_pMesh->GetVertexBuffer(&pVB);
-	pVB->Lock(0,0,(VOID**)&pVer,0);
-	DWORD vsize = m_pMesh->GetNumVertices();
-	for(DWORD n = 0;n < vsize;n++){ //頂点の数を取得する
-		//法線と頂点からuv値を得る
-		BoxVecNomal2UV_1_4(pVer[n].vec,pVer[n].normal,m_Ptn,pVer[n].tu,pVer[n].tv);
-	}
-	pVB->Unlock();
-	//***********************************************************************************
-
-	++m_Ptn;
-}
 
 /**************************************************************************
  Factory_Cursor 定義部
@@ -247,10 +142,9 @@ Factory_Cursor::Factory_Cursor(FactoryPacket* fpac){
 		fpac->m_pVec->push_back(
 			new MouseCursor( 
 						fpac->pD3DDevice,
-						fpac->m_pTexMgr->addTexture(fpac->pD3DDevice, L"MouseCursor.png"),
-						D3DXVECTOR3(90.0f,0.0f,0.0f),
-						D3DXVECTOR3(10.0f,10.0f,0.0f)
-						)
+						fpac->m_pTexMgr
+						
+			)
 		);
 	}
 	catch(...){
--- BomberProject/Factory_Cursor.h
+++ BomberProject/Factory_Cursor.h
@@ -15,6 +15,72 @@
 #include "Object.h"
 
 namespace wiz{
+//	: UIの高さ
+#define UI_HEIGHT					( 88.0f )
+//	: 表示画面の倍率 x=800, y=512 : x=40, y=25.6
+#define DRAW_CLIENT_MAGNIFICATION	( 20.0f )
+//	: 磁界の半径
+#define MAGNETIC_RADIUS				( 0.5f )
+
+// スクリーン座標をワールド座標に変換
+inline D3DXVECTOR3* CalcScreenToWorld(
+   D3DXVECTOR3* pout,
+   int Sx,  // スクリーンX座標
+   int Sy,  // スクリーンY座標
+   float fZ,  // 射影空間でのZ値（0～1）
+   int Screen_w,
+   int Screen_h,
+   D3DXMATRIX* View,
+   D3DXMATRIX* Prj
+) {
+   // 各行列の逆行列を算出
+   D3DXMATRIX InvView, InvPrj, VP, InvViewport;
+   D3DXMatrixInverse( &InvView, NULL, View );
+   D3DXMatrixInverse( &InvPrj, NULL, Prj );
+   D3DXMatrixIdentity( &VP );
+   VP._11 = Screen_w/2.0f; VP._22 = -Screen_h/2.0f;
+   VP._41 = Screen_w/2.0f; VP._42 = Screen_h/2.0f;
+   D3DXMatrixInverse( &InvViewport, NULL, &VP );
+
+   // 逆変換
+   D3DXMATRIX tmp = InvViewport * InvPrj * InvView;
+   D3DXVec3TransformCoord( pout, &D3DXVECTOR3((float)Sx,(float)Sy,fZ), &tmp );
+
+   return pout;
+}
+//スクリーン座標とXZ平面のワールド座標交点算出（ CalcScreenToXZ関数 ）
+// XZ平面とスクリーン座標の交点算出関数
+inline D3DXVECTOR3* CalcScreenToXZ(
+   D3DXVECTOR3* pout,
+   int Sx,
+   int Sy,
+   int Screen_w,
+   int Screen_h,
+   D3DXMATRIX* View,
+   D3DXMATRIX* Prj
+) {
+   D3DXVECTOR3 nearpos;
+   D3DXVECTOR3 farpos;
+   D3DXVECTOR3 ray;
+   CalcScreenToWorld( &nearpos, Sx, Sy, 0.0f, Screen_w, Screen_h, View, Prj );
+   CalcScreenToWorld( &farpos, Sx, Sy, 1.0f, Screen_w, Screen_h, View, Prj );
+   ray = farpos - nearpos;
+   D3DXVec3Normalize( &ray, &ray );
+
+   // 床との交差が起きている場合は交点を
+   // 起きていない場合は遠くの壁との交点を出力
+   if( ray.y <= 0 ) {
+      // 床交点
+      float Lray = D3DXVec3Dot( &ray, &D3DXVECTOR3(0,1,0) );
+      float LP0 = D3DXVec3Dot( &(-nearpos), &D3DXVECTOR3(0,1,0) );
+      *pout = nearpos + (LP0/Lray)*ray;
+   }
+   else {
+      *pout = farpos;
+   }
+
+   return pout;
+}
 
 /**************************************************************************
  MouseCursor 定義部
@@ -25,21 +91,24 @@ namespace wiz{
 // 担当者  : 曳地 大洋
 // 用途    : マウスカーソル
 //**************************************************************************//
-class MouseCursor : public PrimitiveBox{
+class MouseCursor : public PrimitiveBox , public  PrimitiveSprite{
 	int			m_Ptn;
+	float		m_MovePosY;
+
 	LPDIRECT3DTEXTURE9 m_pMouseTex;
 	Camera*	    m_pCamera;
+	D3DXMATRIX	m_mScale;
 
 //protected:
-///////////////////// ////////////////////
-////// 用途       ：MouseCursor(	LPDIRECT3DDEVICE9 pD3DDevice,LPDIRECT3DTEXTURE9 pTexture,wiz::OBJID id = OBJID_3D_WALL);
-////// カテゴリ   ：コンストラクタ
-////// 用途       ：関数
-////// 引数       ：なし
-////// 戻値       ：なし
-////// 担当者     ：鴫原 徹
-////// 備考       ：
-//	void UpdateTargetItem();
+	///////////////////// ////////////////////
+	////// 用途       ：MouseCursor(	LPDIRECT3DDEVICE9 pD3DDevice,LPDIRECT3DTEXTURE9 pTexture,wiz::OBJID id = OBJID_3D_WALL);
+	////// カテゴリ   ：コンストラクタ
+	////// 用途       ：関数
+	////// 引数       ：なし
+	////// 戻値       ：なし
+	////// 担当者     ：鴫原 徹
+	////// 備考       ：
+	//	void UpdateTargetItem();
 
 public:
 	/////////////////// ////////////////////
@@ -52,12 +121,7 @@ public:
 	//// 戻値       ：無し
 	//// 担当者     ：本多寛之
 	//// 備考       ：
-	MouseCursor(LPDIRECT3DDEVICE9  pD3DDevice,
-				LPDIRECT3DTEXTURE9 pTexture,
-				D3DXVECTOR3		   &vScale,
-				D3DXVECTOR3		   &vPos,
-				wiz::OBJID id = OBJID_3D_Cursor
-				);
+	MouseCursor(LPDIRECT3DDEVICE9  pD3DDevice,TextureManager* m_TexMgr);
 
 	//bool HitTest2DRectAndCircle( D3DXVECTOR3& i_vPos, float i_fRadius );
 
