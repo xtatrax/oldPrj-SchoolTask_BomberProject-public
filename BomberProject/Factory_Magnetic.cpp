fb953fe9eef36a1a060e503ad88bfd7d59beb8e7
Ryo <zeal11300019@c-league.jp>
2013-05-30 15:46:04 +0900

    ゴール時のコイルの動き変更

--- BomberProject/Factory_Magnetic.cpp
+++ BomberProject/Factory_Magnetic.cpp
@@ -171,6 +171,11 @@ MagneticumObject3D::MagneticumObject3D(
 		   Diffuse,Specular,Ambient,
 		   id)
 ,m_bMagnetPole( POLE_N )
+,m_pMagneticField ( NULL )
+,m_pMagneticField2( NULL )
+,m_pMagneticField3( NULL )
+,m_pMagneticField4( NULL )
+
 {
 	::ZeroMemory( &m_Material, sizeof(D3DMATERIAL9) ) ;
 }
--- BomberProject/Factory_Magnetic.h
+++ BomberProject/Factory_Magnetic.h
@@ -14,7 +14,6 @@
 #include "StdAfx.h"
 #include "Object.h"
 #include "BassItems.h"
-//#include "Factory_Player.h"
 
 const int	MGPRM_MAGNETICUM			= 10 ; /* Ž¥ŠE‚Ì‰e‹¿”¼Œa*/
 const int	MGPRM_MAGNETICUM_QUAD		= ( MGPRM_MAGNETICUM * MGPRM_MAGNETICUM );
