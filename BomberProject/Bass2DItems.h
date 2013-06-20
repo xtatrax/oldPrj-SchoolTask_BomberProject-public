6c896b413d18ead76f0eaf41686316989336c375
tatra <xtatrax@gmail.com>
2013-06-20 21:57:46 +0900

    解像度のズレの問題を修正した｡ 一時的に無敵GaugeがMAXの時のエフェクトをOFFにした｡

--- BomberProject/Bass2DItems.h
+++ BomberProject/Bass2DItems.h
@@ -182,7 +182,7 @@ public:
 		const D3DXVECTOR3&			vOffsetPos	,
 		const Color					color		= 0xFFFFFFFF		,
 		const wiz::OBJID			id			= OBJID_UI_SPRITE	,
-		const bool					bApplyAspect= false
+		const bool					bApplyAspect= true
 	);
 /////////////////// ////////////////////
 //// ä÷êîñº     ÅF~SpriteObject();
